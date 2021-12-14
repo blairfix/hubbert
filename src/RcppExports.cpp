8c9d0bbae01fac84d7b6b424691096f03eaef6c5
Blair Fix <blairfix@gmail.com>
2021-12-14 05:46:19 -0500

    added lotka-voltera functions

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -36,6 +36,54 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// depletion_stats
+arma::mat depletion_stats(const arma::vec& depletion, const arma::vec& well_id, const arma::vec& month);
+RcppExport SEXP _hubbert_depletion_stats(SEXP depletionSEXP, SEXP well_idSEXP, SEXP monthSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const arma::vec& >::type depletion(depletionSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type well_id(well_idSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type month(monthSEXP);
+    rcpp_result_gen = Rcpp::wrap(depletion_stats(depletion, well_id, month));
+    return rcpp_result_gen;
+END_RCPP
+}
+// lotka_volt
+arma::mat lotka_volt(double r_0, double c_0, double k_1, double k_2, double k_3, double time, double step_size);
+RcppExport SEXP _hubbert_lotka_volt(SEXP r_0SEXP, SEXP c_0SEXP, SEXP k_1SEXP, SEXP k_2SEXP, SEXP k_3SEXP, SEXP timeSEXP, SEXP step_sizeSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< double >::type r_0(r_0SEXP);
+    Rcpp::traits::input_parameter< double >::type c_0(c_0SEXP);
+    Rcpp::traits::input_parameter< double >::type k_1(k_1SEXP);
+    Rcpp::traits::input_parameter< double >::type k_2(k_2SEXP);
+    Rcpp::traits::input_parameter< double >::type k_3(k_3SEXP);
+    Rcpp::traits::input_parameter< double >::type time(timeSEXP);
+    Rcpp::traits::input_parameter< double >::type step_size(step_sizeSEXP);
+    rcpp_result_gen = Rcpp::wrap(lotka_volt(r_0, c_0, k_1, k_2, k_3, time, step_size));
+    return rcpp_result_gen;
+END_RCPP
+}
+// lotka_volt_cumulative
+arma::mat lotka_volt_cumulative(double r_0, double c_0, double k_1, double k_2, double k_3_init, double k_3_slope, double time, double step_size);
+RcppExport SEXP _hubbert_lotka_volt_cumulative(SEXP r_0SEXP, SEXP c_0SEXP, SEXP k_1SEXP, SEXP k_2SEXP, SEXP k_3_initSEXP, SEXP k_3_slopeSEXP, SEXP timeSEXP, SEXP step_sizeSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< double >::type r_0(r_0SEXP);
+    Rcpp::traits::input_parameter< double >::type c_0(c_0SEXP);
+    Rcpp::traits::input_parameter< double >::type k_1(k_1SEXP);
+    Rcpp::traits::input_parameter< double >::type k_2(k_2SEXP);
+    Rcpp::traits::input_parameter< double >::type k_3_init(k_3_initSEXP);
+    Rcpp::traits::input_parameter< double >::type k_3_slope(k_3_slopeSEXP);
+    Rcpp::traits::input_parameter< double >::type time(timeSEXP);
+    Rcpp::traits::input_parameter< double >::type step_size(step_sizeSEXP);
+    rcpp_result_gen = Rcpp::wrap(lotka_volt_cumulative(r_0, c_0, k_1, k_2, k_3_init, k_3_slope, time, step_size));
+    return rcpp_result_gen;
+END_RCPP
+}
 // prod_curve
 arma::mat prod_curve(const arma::uvec& well_id, const arma::vec& prod_first_6, const arma::vec& prod_first_12, const arma::vec& prod_first_24, const arma::vec& prod_first_60, const arma::vec& prod_2nd_last_year, const arma::vec& prod_last_year, const arma::vec& prod_daily_last_year, const arma::vec& prod_cumulative, const arma::vec& prod_peak_daily, const arma::vec& date_start, const arma::vec& date_end, const arma::vec& peak_month, const arma::vec& decline_3, const arma::vec& decline_12, const arma::vec& decline_24, const arma::vec& decline_60, double time_step, double step_threshold);
 RcppExport SEXP _hubbert_prod_curve(SEXP well_idSEXP, SEXP prod_first_6SEXP, SEXP prod_first_12SEXP, SEXP prod_first_24SEXP, SEXP prod_first_60SEXP, SEXP prod_2nd_last_yearSEXP, SEXP prod_last_yearSEXP, SEXP prod_daily_last_yearSEXP, SEXP prod_cumulativeSEXP, SEXP prod_peak_dailySEXP, SEXP date_startSEXP, SEXP date_endSEXP, SEXP peak_monthSEXP, SEXP decline_3SEXP, SEXP decline_12SEXP, SEXP decline_24SEXP, SEXP decline_60SEXP, SEXP time_stepSEXP, SEXP step_thresholdSEXP) {
@@ -69,6 +117,9 @@ END_RCPP
 static const R_CallMethodDef CallEntries[] = {
     {"_hubbert_cumsum_by_tag", (DL_FUNC) &_hubbert_cumsum_by_tag, 2},
     {"_hubbert_cumsum_by_date", (DL_FUNC) &_hubbert_cumsum_by_date, 3},
+    {"_hubbert_depletion_stats", (DL_FUNC) &_hubbert_depletion_stats, 3},
+    {"_hubbert_lotka_volt", (DL_FUNC) &_hubbert_lotka_volt, 7},
+    {"_hubbert_lotka_volt_cumulative", (DL_FUNC) &_hubbert_lotka_volt_cumulative, 8},
     {"_hubbert_prod_curve", (DL_FUNC) &_hubbert_prod_curve, 19},
     {NULL, NULL, 0}
 };
